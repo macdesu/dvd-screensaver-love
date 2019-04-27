ff123a7a6346b57875978d0f2b5c87d9a57159aa
MacDesu <32233686+macdesu@users.noreply.github.com>
2019-04-27 23:53:34 +0200

    Clean code, finally

--- main.lua
+++ main.lua
@@ -1,41 +1,37 @@
 function love.load()
 	logo = love.graphics.newImage("dvd-logo.png")
-	x = 0
-	y = 0
-	speedx = 3
-	speedy = 2
-	hit_counter = -2
-	corner_counter = -1
+	width, height = love.graphics.getDimensions()
+	logo_width, logo_height = logo:getDimensions()
+	posx = 0
+	posy = 0
+	speedx = 10
+	speedy = speedx * 0.8
+	hit_counter = 0
+	corner_counter = 0
 end
 
 function love.update()
-	if x == 0 then
-		speedx = 5
-		hit_counter = hit_counter + 1
-	elseif x == 600 then
-		speedx = -5
-		hit_counter = hit_counter + 1
-	end
-	if y == 0 then
-		speedy = 4
-		hit_counter = hit_counter + 1
-	elseif y == 500 then
-		speedy = -4
+	posx = posx + speedx
+	posy = posy + speedy
+
+	if posx <= 0 or posx >= width - (logo_width / 2) then
+		speedx = -speedx
 		hit_counter = hit_counter + 1
 	end
 
-	if  x == 0 and y == 0 or x == 600 and y == 0 then
+	if posy <= 0 or posy >= height - (logo_height / 2) then
+		speedy = -speedy
+		hit_counter = hit_counter + 1
+	end
+
+	if posx <= 0 and posy <= 0 or posx >= width - logo_width / 2 and posy <= 0 then
 		corner_counter = corner_counter + 1
 	end
-
-	x = x + speedx
-	y = y + speedy
 end
 
 function love.draw()
-	love.graphics.draw(logo, x, y, 0, 0.5, 0.5)
-	love.graphics.print("Corner: " .. corner_counter, 0, 555)
-	love.graphics.print("Bounces: " .. hit_counter, 0, 570)
-	love.graphics.print("x: " .. x .. ", y: " .. y, 0, 585)
-
+	love.graphics.draw(logo, posx, posy, 0, 0.5, 0.5)
+	love.graphics.print("Corner: " .. corner_counter, 0, (height - 45))
+	love.graphics.print("Bounces: " .. hit_counter, 0, (height - 30))
+	love.graphics.print("x: " .. posx .. ", y: " .. posy, 0, (height - 15))
 end
